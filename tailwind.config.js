68fe0af8d2468fe66b3738268dd3318fff75c189
bduble <brianduble77@gmail.com>
2025-06-09 11:46:59 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  content: ["./frontend/src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        electricblue: "#2F80ED",
+        slategray:   "#2D3748",
+        neongreen:   "#00FFC6",
+        offwhite:    "#F7FAFC",
+        darkblue:    "#1C64F2",
+      },
+    },
+  },
+  plugins: [],
+}
