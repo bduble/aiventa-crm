f88885a2259709b6709de97412835c621f479a3a
bduble <brianduble77@gmail.com>
2025-06-12 19:36:27 +0000

    chore: load .env locally only

--- app/db.py
+++ app/db.py
@@ -1,10 +1,12 @@
 # app/db.py
 import os
 from dotenv import load_dotenv
-from supabase import create_client
 
-load_dotenv()   # <-- this loads .env, .env.* etc
+# load local .env (this does nothing in Render, since there is no .env there)
+load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-supabase      = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+from supabase import create_client
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
