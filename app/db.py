2587fb5208f0ca9813f1b76419a99854ed3c91d5
bduble <brianduble77@gmail.com>
2025-06-12 19:21:22 +0000

    chore: stop tracking envfile and load creds from env

--- app/db.py
+++ app/db.py
@@ -1,11 +1,10 @@
 # app/db.py
-from dotenv import load_dotenv
-load_dotenv()            # ← make sure this is *before* any os.environ[...] uses
-
 import os
+from dotenv import load_dotenv
 from supabase import create_client
 
-SUPABASE_URL = os.environ["https://ckdwsvviiuhyqzroswfe.supabase.co"]
-SUPABASE_KEY = os.environ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZHdzdnZpaXVoeXF6cm9zd2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0OTczODgsImV4cCI6MjA2NTA3MzM4OH0.eKOoMaiI6btiJS343Z3-x9ecLwY8MrkVVYY7JoiMI3I"]
+load_dotenv()   # <-- this loads .env, .env.* etc
 
-supabase = create_client(https://ckdwsvviiuhyqzroswfe.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZHdzdnZpaXVoeXF6cm9zd2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0OTczODgsImV4cCI6MjA2NTA3MzM4OH0.eKOoMaiI6btiJS343Z3-x9ecLwY8MrkVVYY7JoiMI3I)
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase      = create_client(SUPABASE_URL, SUPABASE_KEY)
