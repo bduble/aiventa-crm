fd046193c5cd113da8946dda7c7c4881ef1aacc2
bduble <brianduble77@gmail.com>
2025-06-12 18:44:49 +0000

    fix: update asgi import to app.main

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from .routers.leads import router as leads_router
-from .routers.users import router as users_router
+from app.routers import leads, users
 
-app = FastAPI(title="Aiventa CRM API")
+app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(leads_router)
-app.include_router(users_router)
+app.include_router(leads.router)
+app.include_router(users.router)
 
 @app.get("/")
 def read_root():
