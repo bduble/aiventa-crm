6dedcefa8e7f37d28f2c8969da617d0d87e743cf
bduble <brianduble77@gmail.com>
2025-06-12 19:03:20 +0000

    fix(backend): load_dotenv before reading SUPABASE_*

--- app/db.py
+++ app/db.py
@@ -8,4 +8,4 @@ from supabase import create_client
 SUPABASE_URL = os.environ["https://ckdwsvviiuhyqzroswfe.supabase.co"]
 SUPABASE_KEY = os.environ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZHdzdnZpaXVoeXF6cm9zd2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0OTczODgsImV4cCI6MjA2NTA3MzM4OH0.eKOoMaiI6btiJS343Z3-x9ecLwY8MrkVVYY7JoiMI3I"]
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(https://ckdwsvviiuhyqzroswfe.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZHdzdnZpaXVoeXF6cm9zd2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0OTczODgsImV4cCI6MjA2NTA3MzM4OH0.eKOoMaiI6btiJS343Z3-x9ecLwY8MrkVVYY7JoiMI3I)
