98075ee01706430f5ad12b7b5fdd897d6ffc1f52
bduble <brianduble77@gmail.com>
2025-06-16 13:35:12 -0500

    Create ActivityTimeline.jsx

---
+++ frontend/src/components/ActivityTimeline.jsx
@@ -0,0 +1,11 @@
+// frontend/src/components/ActivityTimeline.jsx
+import React from 'react'
+
+export default function ActivityTimeline() {
+  return (
+    <div>
+      <h2>Activity Timeline</h2>
+      {/* we’ll hook this up to /activities soon */}
+    </div>
+  )
+}
