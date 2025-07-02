1b09f5587453b8c4b7189d2f92f359b42cfcfb08
bduble <brianduble77@gmail.com>
2025-07-02 13:26:58 -0500

    Use public logo asset

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from '../assets/logo.png';
 
 export default function Logo({ className = '' }) {
   return (
     <span className={`inline-flex items-center justify-center ${className}`.trim()}>
-      <img src={logo} alt="aiVenta logo" className="h-12 w-auto md:h-14" />
+      <img src="/logo.png" alt="aiVenta logo" className="h-12 w-auto md:h-14" />
     </span>
   );
 }
