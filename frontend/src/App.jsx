c4a6bcd15ed513c7a0a1468b382faf86d909eca7
bduble <brianduble77@gmail.com>
2025-07-01 22:21:06 -0500

    Add Lead Log API and React page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import Leads                  from "./routes/Leads";
+import LeadLog                from "./routes/LeadLog";
 import Users                  from "./routes/Users";
 import ActivityTimeline       from "./components/ActivityTimeline";
 import CreateLeadForm         from "./components/CreateLeadForm";
@@ -79,7 +79,7 @@ export default function App() {
       <div style={contentWrapperStyle}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/leads" element={<Leads />} />
+          <Route path="/leads" element={<LeadLog />} />
           <Route path="/leads/new" element={<CreateLeadForm />} />
           <Route path="/users" element={<Users />} />
           <Route path="/activities" element={<ActivityTimeline />} />
