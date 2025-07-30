a1c0f4c47d6d82ea295e10460eb2bce7ad0eab28
bduble <brianduble77@gmail.com>
2025-07-30 15:12:27 -0500

    Update CustomerNameLink.jsx

--- frontend/src/components/CustomerNameLink.jsx
+++ frontend/src/components/CustomerNameLink.jsx
@@ -3,6 +3,7 @@ import { useCustomerCard } from '../context/CustomerCardContext';
 
 export default function CustomerNameLink({ id, name }) {
   const { open } = useCustomerCard();
+  console.log('[CustomerNameLink] id:', id, 'name:', name);
 
   if (!id) return <span>{name || ''}</span>;
 
